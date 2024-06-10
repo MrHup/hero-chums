4c4f8def27a9488f3c8892063cdab151771749af
MrHup <hup.hubspace@gmail.com>
2024-06-10 21:45:24 +0300

    Add styling to google map

--- lib/map_widget.dart
+++ lib/map_widget.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapSample extends StatefulWidget {
@@ -14,61 +16,71 @@ class MapSampleState extends State<MapSample> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
 
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
   Set<Marker> _markers = {};
-
-  @override
-  void initState() {
-    super.initState();
-    _addCustomMarker();
-  }
-
-  void _addCustomMarker() async {
-    final marker = Marker(
-      markerId: MarkerId('leaf_marker'),
-      position: LatLng(37.43296265331129, -122.08832357078792),
-      visible: true,
-      icon: await BitmapDescriptor.fromAssetImage(
-          ImageConfiguration(), 'assets/images/leaf_mark.png'),
-    );
-
-    setState(() {
-      print("Added leaf");
-      _markers.add(marker);
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
-        mapType: MapType.normal,
-        initialCameraPosition: _kGooglePlex,
-        markers: _markers,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
+        onMapCreated: (GoogleMapController c) {
+          changeMapMode(c);
+          _controller.complete(c);
         },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
-        icon: const Icon(Icons.directions_boat),
+        mapType: MapType.normal,
+        trafficEnabled: false,
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(45.756685, 21.229283),
+          zoom: 13.75,
+        ),
+        markers: _markers,
+        zoomControlsEnabled: false,
+        // zoomGesturesEnabled: false,
+        cameraTargetBounds: CameraTargetBounds(
+          LatLngBounds(
+            northeast: const LatLng(45.812293, 21.348564),
+            southwest: const LatLng(45.693001, 21.150284),
+          ),
+        ),
+
+        onTap: _handleTap,
       ),
     );
   }
 
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  // Handles the tap on the map
+  void _handleTap(LatLng tappedPoint) {
+    _addCustomMarker(tappedPoint);
+  }
+
+  // Add a custom marker at the tapped location
+  void _addCustomMarker(LatLng location) async {
+    final marker = Marker(
+      markerId: MarkerId(location.toString()), // Ensure unique ID
+      position: location,
+      icon: await BitmapDescriptor.fromAssetImage(
+          const ImageConfiguration(size: Size(48, 68)),
+          'assets/images/leaf_mark.png'),
+    );
+
+    setState(() {
+      _markers.add(marker);
+    });
+  }
+
+  void changeMapMode(GoogleMapController mapController) {
+    getJsonFile("map_styles/style_silver.json")
+        .then((value) => setMapStyle(value, mapController));
+  }
+
+  //helper function
+  void setMapStyle(String mapStyle, GoogleMapController mapController) {
+    mapController.setMapStyle(mapStyle);
+  }
+
+  //helper function
+  Future<String> getJsonFile(String path) async {
+    ByteData byte = await rootBundle.load(path);
+    var list = byte.buffer.asUint8List(byte.offsetInBytes, byte.lengthInBytes);
+    return utf8.decode(list);
   }
 }
