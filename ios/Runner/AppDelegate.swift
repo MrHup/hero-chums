7b376b920a4ea600e7a061487481f42d10e5c0e0
MrHup <hup.hubspace@gmail.com>
2024-08-07 17:35:02 +0300

    Remove outdated key

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDzLslZXWO7pKM8ebuwpVC_e7wnKv_cELo")
+    GMSServices.provideAPIKey("key_here")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
